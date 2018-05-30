9a3f5ab97a1fe9acb697043007cf8195a804a69f
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-05-30 13:13:04 +0200

    getting files tests

--- BlessFPS/src/main/java/main.java
+++ BlessFPS/src/main/java/main.java
@@ -2,6 +2,12 @@
 import java.awt.FlowLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -21,7 +27,42 @@ public class main {
         JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
     }
 
+    private static void getAllData() {
+        try {
+            //getting file
+            String line;
+            ArrayList<String> lines = new ArrayList<>();
+            File f1 = new File("S:\\SteamLibrary\\steamapps\\common\\Bless Online\\Engine\\Config\\BaseEngine.ini");
+            FileReader fr = new FileReader(f1);
+            BufferedReader br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+            fr.close();
+            br.close();
+
+            System.out.println(lines.get(3));
+
+            //putting file
+            FileWriter fw = new FileWriter(f1);
+            BufferedWriter out = new BufferedWriter(fw);
+            for (String s : lines) {
+                out.write(s);
+            }
+            out.flush();
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static String blessPath;
+
     public static void main(String s[]) {
+        //testing
+        getAllData();
+        //endof testing
+
         JFrame frame = new JFrame("BlessFPS");
         DemoJFileChooser panel = new DemoJFileChooser();
         frame.addWindowListener(
@@ -37,9 +78,11 @@ public class main {
         frame.setSize(panel.getPreferredSize());
         frame.setVisible(true);
         while (true) { //i know this is bad code, but i want to get it working before opimtimizing and i havent used java in a long time
-            System.out.println(panel.path);
-            if (panel.path != null) {
-                if (panel.path.endsWith("\\steamapps\\common\\Bless Online")) {
+            blessPath = panel.path;
+            //for some reason only after printing this the if statement will work, wtf java
+            System.out.println(blessPath);
+            if (blessPath != null) {
+                if (blessPath.endsWith("\\steamapps\\common\\Bless Online")) {
                     break;
                 } else {
                     infoBox("The path you chose was not a compatible path. Please try again.\nYour selected path was: " + panel.path, "Path finding error");
@@ -48,7 +91,7 @@ public class main {
             }
         }
         frame.getContentPane().removeAll();
-        frame.add(new JLabel("test"));
+        frame.add(new JLabel("TODO optimization"));
         frame.revalidate();
         frame.repaint();
 
